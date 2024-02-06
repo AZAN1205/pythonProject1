4f85e419290df3bd5d111db23ae3b0c04e3b7f8f
AZAN1205 <156790580+AZAN1205@users.noreply.github.com>
2024-02-06 20:57:43 +0530

    Add files via upload

---
+++ Prectice.py
@@ -0,0 +1,9 @@
+a = int(input("Enter any Number"))
+if a >= 60:
+    print(" I Like Apple")
+elif a >= 30:
+    print("I Like Chocolate")
+elif a >= 20:
+    print(" like Ice Cream")
+else:
+    print("I do not like Anything")
